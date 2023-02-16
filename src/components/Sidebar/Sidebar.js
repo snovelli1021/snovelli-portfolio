4821ff05361b4831a626e3f6b24444800365cca2
Stephen Novelli <snovelli1021@gmail.com>
2023-02-15 19:54:37 -0800

    Added content and links to sidebar, styled and positioned button and links with a temporary theme.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import "./Sidebar.css";
+import data from "./sidebarData.json";
 
-//  Need to style and position the button (Upper Left) for the sidebar. Thinking about using the information icon.
-function Sidebar({ name }) {
+function Sidebar() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -11,15 +11,42 @@ function Sidebar({ name }) {
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow} className="me-2">
-        {name}
-      </Button>
+      {/* Need to style button with theme. */}
+
+      <svg
+        onClick={handleShow}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="currentColor"
+        className="bi bi-info-square-fill"
+        viewBox="0 0 16 16"
+      >
+        <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.93 4.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+      </svg>
+
+      {/* Need to style text and links, maybe add a background image? */}
+
       <Offcanvas show={show} onHide={handleClose}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>About</Offcanvas.Title>
+          <Offcanvas.Title>Additional Information</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          Add detailed text about each page and maybe online profiles?
+          <div>
+            <p>
+              Couldn’t find what you’re looking for? Follow the links below to
+              learn more about me and my work!
+            </p>
+            {data.map(({ id, url, profile }) => (
+              <ul key={id} className="profileLinks">
+                <li>
+                  <a href={url} alt="Link to online profile" target="blank">
+                    {profile}
+                  </a>
+                </li>
+              </ul>
+            ))}
+          </div>
         </Offcanvas.Body>
       </Offcanvas>
     </>
