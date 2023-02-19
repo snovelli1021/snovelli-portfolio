00790ef76fc5aae0b79c15eb618f89f19f6e6681
Stephen Novelli <snovelli1021@gmail.com>
2023-02-18 21:14:36 -0800

    Changed idea for button to render cards, added cards for bio, work, and education to a carousel. Need to style and rework some text.

--- src/components/pages/AboutMe/AboutMe.js
+++ src/components/pages/AboutMe/AboutMe.js
@@ -1,8 +1,10 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
+import Carousel from "react-bootstrap/Carousel";
 import "./AboutMe.css";
 import bioData from "./bioData.json";
+import workData from "./workData.json";
+import eduData from "./eduData.json";
 
 const AboutMe = () => {
   return (
@@ -10,34 +12,67 @@ const AboutMe = () => {
 
     <div>
       <h1>About Me</h1>
-      {bioData.map(({ id, bioPhoto, bioTitle, bioText }) => (
-        <Card key={id} className="contentCard" style={{ width: "18rem" }}>
-          <Card.Img
-            variant="top"
-            src={bioPhoto}
-            id="bioPhoto"
-            alt="Screenshot of website or application"
-          />
-          <Card.Body>
-            <Card.Title>{bioTitle}</Card.Title>
-            <Card.Text>{bioText}</Card.Text>
-          </Card.Body>
 
-          {/* buttons will render cards for bio, work, and education experience.  */}
+      <Carousel interval={null}>
+        <Carousel.Item interval={null} className="carouselItem">
+          {bioData.map(({ id, bioPhoto, bioTitle, bioText }) => (
+            <Card key={id} className="bioCard" style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src={bioPhoto}
+                id="bioPhoto"
+                alt="Profile Picture of Stephen Novelli"
+              />
+              <Card.Body>
+                <Card.Title>{bioTitle}</Card.Title>
+                <Card.Text>{bioText}</Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
+        </Carousel.Item>
 
-          <div className="btnGroup">
-            <Button id="bioBtn" variant="primary">
-              1
-            </Button>
-            <Button id="workBtn" variant="primary">
-              2
-            </Button>
-            <Button id="eduBtn" variant="primary">
-              3
-            </Button>
-          </div>
-        </Card>
-      ))}
+        {/* Need to edit card text. */}
+
+        <Carousel.Item interval={null} className="carouselItem">
+          {workData.map(
+            ({ id, workPhoto, workTitle, workText, workFooter }) => (
+              <Card key={id} className="workCard" style={{ width: "18rem" }}>
+                <Card.Img
+                  variant="top"
+                  src={workPhoto}
+                  className="workPhoto"
+                  alt="Screenshots of company logos of previsous and current employers"
+                />
+                <Card.Body>
+                  <Card.Title>{workTitle}</Card.Title>
+                  <Card.Text>{workText}</Card.Text>
+                  <p>{workFooter}</p>
+                </Card.Body>
+              </Card>
+            )
+          )}
+        </Carousel.Item>
+
+        {/* Need to edit card text. */}
+
+        <Carousel.Item interval={null} className="carouselItem">
+          {eduData.map(({ id, eduPhoto, eduTitle, eduText, eduFooter }) => (
+            <Card key={id} className="workCard" style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src={eduPhoto}
+                className="eduPhoto"
+                alt="Screenshots of company logos of previsous and educational institutions"
+              />
+              <Card.Body>
+                <Card.Title>{eduTitle}</Card.Title>
+                <Card.Text>{eduText}</Card.Text>
+                <p>{eduFooter}</p>
+              </Card.Body>
+            </Card>
+          ))}
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
