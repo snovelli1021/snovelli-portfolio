897ee8064d456de834728c82f60c609e1a7adeba
Stephen Novelli <snovelli1021@gmail.com>
2023-02-22 13:12:30 -0800

    Added Resume page and buttons for downloadable versions. Need to style and add more content still.

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -1,9 +1,29 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import "./Resume.css";
 
 const Resume = () => {
   return (
+    // Need to finalize resume and add downloadable versions.
+    // Need to style page, and come up with text/images to fill white space.
+
     <div>
       <h1>Resume</h1>
+      <Card className="resumeCard" style={{ width: "20rem" }}>
+        <Card.Body>
+          <Card.Title>Resume</Card.Title>
+          <Card.Text>Resume Text</Card.Text>
+          <p>Click or tap below to download a copy</p>
+          <div className="downloadIcons">
+            <a href="./assets/NovelliResume.docx" download>
+              <button varient="top" id="msWordBtn" alt="Microsoft Word Icon" />
+            </a>
+            <a href="./assets/NovelliResume.pdf" download>
+              <button varient="top" id="adobeBtn" alt="Adobe PDF Icon" />
+            </a>
+          </div>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
