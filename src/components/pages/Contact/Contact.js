aaf0bfcc7ac0ee49636a26248f25814774268645
Stephen Novelli <snovelli1021@gmail.com>
2023-02-23 13:40:20 -0800

    Added email form to Contact Page using emailJS. Need to validate inputs and style.

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,10 +1,76 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "./Contact.css";
+
+// Need to validate form and style.
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("Message sent!");
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <div>
-      <h1>Contact Information</h1>
-    </div>
+    <Form
+      className="contactForm"
+      style={{ width: "20rem" }}
+      ref={form}
+      onSubmit={sendEmail}
+    >
+      <Form.Label>Name</Form.Label>
+      <input
+        className="mb-3"
+        placeholder="First and last name"
+        type="text"
+        name="user_name"
+      />
+      <Form.Label>Email</Form.Label>
+      <input
+        className="mb-3"
+        placeholder="example@email.com"
+        type="email"
+        name="user_email"
+      />
+      <div>
+        <Form.Text className="text-muted">
+          Your information is private and never sharaed.
+        </Form.Text>
+      </div>
+      <Form.Label>Message</Form.Label>
+      <textarea
+        id="messageInput"
+        maxLength={500}
+        placeholder="Type your message here."
+        name="message"
+      />
+      <div>
+        <Form.Text className="text-muted">Maximum 500 characters.</Form.Text>
+      </div>
+      <Button id="formBtn" variant="primary" type="submit">
+        Submit!
+      </Button>
+    </Form>
   );
 };
 
